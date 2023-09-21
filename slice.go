a845ad2b843417e5c36e5c787daded5014a66088
Stefan Utamaru <mail@stenyan.jp>
2023-09-22 00:29:22 +0900

    remove alias

--- slice.go
+++ slice.go
@@ -1,11 +1,11 @@
 package godash
 
-import slo "github.com/samber/lo"
+import "github.com/samber/lo"
 
 func Uniq[T comparable](collection []T) []T {
-	return slo.Uniq(collection)
+	return lo.Uniq(collection)
 }
 
 func UniqBy[T any, U comparable](collection []T, iteratee func(item T) U) []T {
-	return slo.UniqBy(collection, iteratee)
+	return lo.UniqBy(collection, iteratee)
 }
