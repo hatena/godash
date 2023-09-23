5af7e81662ef190b14ce5789044b0de0dd36eda2
Stefan Utamaru <mail@stenyan.jp>
2023-09-23 20:42:18 +0900

    add more functions that would probably be used a bunch

--- godash.go
+++ godash.go
@@ -1,6 +1,9 @@
 package godash
 
-import "github.com/samber/lo"
+import (
+	"github.com/samber/lo"
+	"golang.org/x/exp/constraints"
+)
 
 func Filter[V any](collection []V, predicate func(item V, index int) bool) []V {
 	return lo.Filter(collection, predicate)
@@ -8,6 +11,10 @@ func Filter[V any](collection []V, predicate func(item V, index int) bool) []V {
 
 func Map[T any, R any](collection []T, iteratee func(item T, index int) R) []R {
 	return lo.Map(collection, iteratee)
+}
+
+func Reduce[T any, R any](collection []T, accumulator func(agg R, item T, index int) R, initial R) R {
+	return lo.Reduce(collection, accumulator, initial)
 }
 
 func Uniq[T comparable](collection []T) []T {
@@ -36,4 +43,32 @@ func Flatten[T any](collection [][]T) []T {
 
 func KeyBy[K comparable, V any](collection []V, iteratee func(item V) K) map[K]V {
 	return lo.KeyBy(collection, iteratee)
+}
+
+func Keys[K comparable, V any](in map[K]V) []K {
+	return lo.Keys(in)
+}
+
+func Values[K comparable, V any](in map[K]V) []V {
+	return lo.Values(in)
+}
+
+func Sum[T constraints.Float | constraints.Integer | constraints.Complex](collection []T) T {
+	return lo.Sum(collection)
+}
+
+func SumBy[T any, R constraints.Float | constraints.Integer | constraints.Complex](collection []T, iteratee func(item T) R) R {
+	return lo.SumBy(collection, iteratee)
+}
+
+func EveryBy[T any](collection []T, predicate func(item T) bool) bool {
+	return lo.EveryBy(collection, predicate)
+}
+
+func NoneBy[T any](collection []T, predicate func(item T) bool) bool {
+	return lo.NoneBy(collection, predicate)
+}
+
+func Find[T any](collection []T, predicate func(item T) bool) (T, bool) {
+	return lo.Find(collection, predicate)
 }
